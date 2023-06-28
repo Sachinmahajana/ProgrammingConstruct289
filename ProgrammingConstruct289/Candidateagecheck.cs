ad615b6892bc8568044ee1ee24eadf2906e753b1
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-28 21:10:03 +0530

    [Sachin] Adding CandidateAgeEligible

---
+++ ProgrammingConstruct289/Candidateagecheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct289
+{
+    internal class Candidateagecheck
+    {
+        int number;
+        public void CandidateAgeFunction()
+        {
+            Console.WriteLine("Enter the Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if(number < 18)
+            {
+                Console.WriteLine("Sorry you are not eligible to cast your vote");
+                Console.WriteLine("You would be able to caste your vote after {0} year", 18 - number);
+            }
+            else
+            {
+                Console.WriteLine("Congratulation . You are eligible for casting your vote");
+            }
+        }
+    }
+}
--- ProgrammingConstruct289/Program.cs
+++ ProgrammingConstruct289/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to the Programming construct Program");
             Console.WriteLine("Please choose the program given below");
-            Console.WriteLine("\n1:CheckEqual\n2:Evenoddnumber");
+            Console.WriteLine("\n1:CheckEqual\n2:Evenoddnumber\n3:Candidateagechecking");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -18,6 +18,10 @@
                 case 2:
                     EvenOdd myObj1 = new EvenOdd();
                     myObj1.EvenOddFunction();
+                    break;
+                case 3:
+                    Candidateagecheck myObj2 = new Candidateagecheck();
+                    myObj2.CandidateAgeFunction();
                     break;
             }
         }
