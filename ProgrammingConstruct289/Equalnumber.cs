97d77a232671f43bdd3326f872f7beb6c4e9c963
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-28 19:07:26 +0530

    [Sachin] Adding CheckEqualorNot

---
+++ ProgrammingConstruct289/Equalnumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct289
+{
+    internal class Equalnumber
+    {
+        int num1, num2;  
+        public void EqualNumberFunction()
+        {
+            Console.WriteLine("Enter the Num1 and Num2");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if(num1 == num2)
+            {
+                Console.WriteLine("Number is Equal ");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Equal");
+            }
+        }
+    }
+}
--- ProgrammingConstruct289/Program.cs
+++ ProgrammingConstruct289/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Programming construct Program");
+            Console.WriteLine("Please choose the program given below");
+            Console.WriteLine("\n1:CheckEqual");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Equalnumber myObj = new Equalnumber();
+                    myObj.EqualNumberFunction();
+                    break;
+            }
         }
     }
 }
