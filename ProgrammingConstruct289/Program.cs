c908e31f1b6bb83863be3b325dd057391dcc3437
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-28 18:13:54 +0530

    [Sachin] Adding Welcome Message

---
+++ ProgrammingConstruct289/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgrammingConstruct289
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Programming construct Program");
+        }
+    }
+}
